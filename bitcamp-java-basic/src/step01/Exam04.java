28ecec708118381dfeef854e912bc2f2107a9d04
hongjungho <cvb1221@naver.com>
2018-02-28 17:24:14 +0900

    test

---
+++ bitcamp-java-basic/src/step01/Exam04.java
@@ -0,0 +1,23 @@
+package step01;
+
+public class Exam04 {
+    public static void main(String[] args) {
+        // 숫자 
+        System.out.println(10l);
+        System.out.println(+10l);
+        System.out.println(-10l);
+        System.out.println(3.14l);
+
+        // 문자열
+        System.out.println("오호라..");
+
+        // 한 개의 문자
+        System.out.println('가');
+
+        // 논리값 
+        System.out.println(true);
+        System.out.println(false);
+
+
+    }
+}
---
+++ bitcamp-java-basic/src/step02/Exam01.java
@@ -0,0 +1,6 @@
+package step01;
+public class Exam01 {
+    public static void main(String[] arg){
+        System.out.println("src/step02/Examzz");
+    }
+}
